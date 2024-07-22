87f190b87ca88b3925422e457d2d355edd81ba39
alielsaaed <alielsaad511@gmail.com>
2024-07-22 17:38:46 +0300

    ali's edits

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:js';
+// import 'dart:js';
 
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
@@ -7,15 +7,14 @@ import 'package:provider/provider.dart';
 import 'package:task2_deltana/View/CashOutView.dart';
 import 'package:task2_deltana/ViewModel/Order_ViewModel.dart';
 
-void main() => runApp(
-  ChangeNotifierProvider(create: (context)=> OrderViewModel(),
-  child: DevicePreview(
-    builder: (context) => MyApp(),
-  ),
-  )
-);
+void main() => runApp(ChangeNotifierProvider(
+      create: (context) => OrderViewModel(),
+      child: DevicePreview(
+        builder: (context) => MyApp(),
+      ),
+    ));
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
@@ -28,7 +27,6 @@ class MyApp extends StatelessWidget{
         debugShowCheckedModeBanner: false,
         home: CashOutView(),
       ),
-
     );
   }
 }
